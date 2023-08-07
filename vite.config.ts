ff808191f9d00b666775d20986cb2b714f9d39f5
samhepburn-98 <samhepburn98@msn.com>
2023-08-07 12:05:44 +0100

    feat: update vite config base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from 'path';
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/systemwork-ui/",
     plugins: [react()],
     resolve: {
         alias: {
