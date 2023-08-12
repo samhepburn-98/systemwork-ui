b80c75b591fdba74b46684f0e7998531f269b001
samhepburn-98 <samhepburn98@msn.com>
2023-08-12 14:47:32 +0100

    fix: add vite config base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // base: "/systemwork-ui/",
+    base: "/systemwork-ui/",
     plugins: [react()],
     resolve: {
         alias: {
